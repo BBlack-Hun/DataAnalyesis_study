18f5180af674b788069fda6624a8830beab8d24c
BBlack_Hun <BBlack_Hun@kangwon.ac.kr>
2021-01-31 11:54:15 +0900

    study_1/31

--- data11.py
+++ data11.py
@@ -93,4 +93,65 @@ plt.grid()
 plt.show()
 # %%
 ### 제주도의 연령대별 성별 비율을 산점도로 표현하기
+import csv
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-white')
 
+f = open('age3.csv')
+data = csv.reader(f)
+m = []
+f = []
+
+name = input('궁금한 동네를 입력해주세요!')
+for row in data:
+    if name in row[0]:
+        for i in range(3, 104):
+            m.append(int(row[i].replace(',','')))
+            f.append(int(row[i+103].replace(',','')))
+        break
+
+plt.scatter(m,f, c=range(101), alpha=.5, cmap='jet')
+plt.grid()
+plt.colorbar()
+plt.plot(range(max(m)), range(max(m)), 'g')
+plt.show()
+
+# %%
+### 제주도의 연령대별 성별 비율을 산점도로 표현하기
+### 필요한 모듈 import
+import csv
+import math
+import matplotlib.pyplot as plt
+
+### 파일을 열어서 data에 저장
+f = open('age3.csv')
+data = csv.reader(f)
+
+### 성별 값을 저장하기 위한 리스트 선언
+m = list()
+f = list()
+size = list()
+
+### 지역을 입력받기 위한 입력문
+name = input('궁금한 동네를 입력해주세요: ')
+
+### 파일을 열어 입력 받은 데이터를 한줄씩 반복문을 통해 실행
+for row in data:
+    ### 사용자가 입력한 지역이 존재할 경우
+    if name in row[0]:
+        for i in range(3, 104):
+            m.append(int(row[i].replace(',','')))
+            f.append(int(row[i+103].replace(',','')))
+            size.append(math.sqrt(int(row[i].replace(',',''))+ int(row[i+103].replace(',','')) ))
+        break
+plt.style.use('ggplot')
+plt.rc('font', family='D2Coding')
+plt.figure(figsize=(10,5), dpi=300)
+plt.title(name +' 지역의 성별 인구 그래프')
+plt.scatter(m, f, s=size, c=range(101), alpha=.5, cmap='jet')
+plt.colorbar()
+plt.plot(range(max(m)), range(max(m)), 'g')
+plt.xlabel('남성 인구수')
+plt.ylabel('여성 인구수')
+plt.show()
+# %%
