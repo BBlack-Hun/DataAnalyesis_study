107b276ff2bb5acea91c1db515c9503d3eb87723
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-02-05 08:40:32 +0900

    test commit

--- data12.py
+++ data12.py
@@ -5,3 +5,19 @@ data = csv.reader(f)
 
 for row in data:
     print(row)
+
+# %%
+import csv
+
+f = open('subway.csv')
+data =csv.reader(f)
+next(data)
+for row in data:
+    for i in range(4, 8):
+        row[i] = int(row[i])
+    print(row)
+# %%
+import csv
+import matplotlib.pyplot as plt
+
+
