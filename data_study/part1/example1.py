20d6931d0799030fd7395f8a221c4ede850c9e28
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-04-03 01:45:56 +0900

    04_03_study

--- data_study/part1/example1.py
+++ data_study/part1/example1.py
@@ -401,3 +401,36 @@ print('\n')
 df = df.T
 print(df)
 # %%
+# 특정 열을 인덱스로 설정
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+# DataFrame() 함수로 데이터프레임 변환. 변수 df에 저장
+exam_data = {'이름' : ['서준', '우현', '인아'],
+             '수학' : [90, 80, 70],
+             '영어' : [98, 89, 95],
+             '음악' : [85, 95, 100],
+             '체육' : [100, 90, 90]}
+
+df = pd.DataFrame(exam_data)
+print(df)
+print('\n')
+
+# 특정 열(columns)을 데이터프레임 행 인덱스로 설정
+ndf = df.set_index(['이름'])
+print(ndf)
+print('\n')
+ndf2 = ndf.set_index('음악')
+print(ndf2)
+print('\n')
+ndf3 = ndf.set_index(['수학', '음악'])
+print(ndf3)
+# %%
+# 행 인덱스 재배열
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+# 딕셔너리 정의
+dict_data = {'c0':[1,2,3], 'c1':[4,5,6], 'c2':[7,8,9] , }
