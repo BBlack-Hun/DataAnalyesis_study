ef36ca5e29189fc7ac97c1204011f8a326264fdb
BBlack_Hun <h1103j@kangwon.ac.kr>
2021-04-02 23:40:33 +0900

    04_02_study

--- data_study/part1/example1.py
+++ data_study/part1/example1.py
@@ -97,3 +97,61 @@ print('\n')
 print(df.index)
 print('\n')
 print(df.columns)
+# %%
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+# 행 인덱스 / 열 이름 지정하여 데이터 프레임 만들기
+df = pd.DataFrame([[15, '남', '덕영중'], [17, '여', '수리중']],
+                    index=['준서', '예은'],
+                    columns=['나이','성별', '학교'])
+
+# 데이터프레임 df 출력
+print(df)
+print('\n')
+
+# 열 이름 중, '나이'를 '연령'으로, '성별'을 '남녀'로, '학교'를 '소속'으로 바꾸기
+df.rename(columns={'나이':'연령', '성별':'남녀', '학교':'소속'}, inplace=True)
+
+# df의 행 인덱스 중에서, '준서'를 '학생1'로 '예은'을 '학생2'로 바꾸기
+df.rename(index={'준서':'학생1', '예은':'학생2'}, inplace=True )
+
+# df 출력(변경 후)
+print(df)
+
+# %%
+# 행 삭제: DataFrame 객체.drop(행 인덱스 또는 배열, axis=0)
+# 열 삭제: DataFrame 객체.drop(열 인덱스 또는 배열, axis=1)
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+# DataFrame() 함수로 데이터프레임 변환. 변수 df에 저장
+exam_data = {'수학':[90, 80, 70], '영어':[98,89,95],
+             '음악':[85, 95,100], '체육':[100, 90, 90]}
+
+df = pd.DataFrame(exam_data, index=['서준', '우현', '인아'])
+print(df)
+print('\n')
+
+# 데이터 프레임 df를 복제하여 변수 df2에 저장. df2의 1개 행(row) 삭제
+df2 = df[:]
+df2.drop('우현', inplace=True)
+print(df2)
+print('\n')
+
+# 데이터 프레임 df를 복제하여 변수 df3에 저장. df3의 2개 행(row)삭제
+df3 = df[:]
+df3.drop(['우현','인아'], axis=0, inplace=True)
+print(df3)
+print('\n')
+
+# %%
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+# DataFrame() 함수로 데이터프렝림 변환, 변수 df에 저장.
+exam_data = {'수학':[90,80,70], '영어':[98,89,95],
+             '음악':[85,95,]]}
