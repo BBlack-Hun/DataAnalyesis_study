7b6fb19f05aaaeba26700d6bbf583d710ac65acc
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-04 13:06:01 +0900

    04_04_study

--- data_study/part1/example1.py
+++ data_study/part1/example1.py
@@ -602,3 +602,29 @@ print('\n')
 print(type(addiction))
 
 # %%
+# 데이터 프레임끼리 더하기
+# -*- coding: utf-8 -*-
+
+# 라이브러리 불러오기
+
+# titanic 데이터셋에서 aqe, fare 2개 열을 선택하여 데이터프레임 만들기
+titanic = sns.load_dataset('titanic')
+df = titanic.loc[:, ['age', 'fare']]
+print(df)
+print('\n')
+print(type(df))
+print('\n')
+
+# 데이터프레임에 숫자 10 더하기
+addcition = df + 10
+print(addiction.tail())
+print('\n')
+print(type(addiction))
+print('\n')
+
+# 데이터프레임끼리 연산하기( addiction - df)
+substraction = addcition - df
+print(substraction.tail())
+print('\n')
+print(type(substraction))
+# %%
