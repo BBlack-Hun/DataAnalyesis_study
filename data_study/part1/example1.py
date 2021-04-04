0947a7a55038e998525e4b31680b6277800617e6
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-04 13:01:42 +0900

    04_04_study

--- data_study/part1/example1.py
+++ data_study/part1/example1.py
@@ -588,4 +588,17 @@ print(result)
 # 라이브러리 불러오기
 
 # titanic 데이터셋에서 age, fare 2개 열을 선택하여 데이터프레임 만들기
-titianic = sns.
+titianic = sns.load_dataset('titanic')
+df = titianic.loc[:, ['age', 'fare']]
+print(df.head())
+print('\n')
+print(type(df))
+print('\n')
+
+# 데이터프레임에 숫자 10 더하기
+addiction = df + 10
+print(addiction.head())
+print('\n')
+print(type(addiction))
+
+# %%
